c4181ae4670bd565ae4264378ed57cc4faf1308b
Naveen-131 <naveenmadhavan131@gmail.com>
2025-01-06 22:30:42 +0530

    Updated LinkedIn profile button

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -30,7 +30,7 @@ const Navbar = () => {
           <NavLink href='#contact'>Contact</NavLink>
         </NavItems>
         <ButtonContainer>
-          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          <GitHubButton href={Bio.linkedin} target="_blank">LinkedIn Profile</GitHubButton>
         </ButtonContainer>
         {
           isOpen &&
@@ -50,7 +50,7 @@ const Navbar = () => {
             <MobileLink href='#contact' onClick={() => {
                 setIsOpen(!isOpen)
             }}>Contact</MobileLink>
-            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.linkedin} target="_blank">Linkedin Profile</GitHubButton>
           </MobileMenu>
         }
 
